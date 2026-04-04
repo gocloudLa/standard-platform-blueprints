22658073e0a8f0a86562ddf87f48ad2b0cc627eb
Federico Midaglia <fmidaglia@gocloud.la>
2026-04-04 13:29:40 -0300

    feat(vpn): add site2site vpn

--- networking-blueprint/base/lab_vpc_net/main.tf
+++ networking-blueprint/base/lab_vpc_net/main.tf
@@ -184,4 +184,78 @@ module "base" {
       }
     }
   }
+
+
+  vpn_parameters = {
+    "vpn-01" = {
+
+      vpc = "networking" # Key into vpc_parameter (not vpc_name)
+      # tgw = "tgw-01" # Key into tgw_parameter (not tgw_name)
+
+      # Can be alternated between them if necessary
+      # vpc_id = null
+      # tgw_id = null
+
+      # If resources already exist you can pass their ID; otherwise leave null and fill the rest for a custom configuration; if the object is null defaults apply
+      virtual_private_gateway = {
+        # amazon_side_asn = 64512
+        # availability_zone = null
+
+        # virtual_private_gateway_id = null
+      }
+      customer_gateway = {
+        ip_address = "123.123.123.123" // Required, Public IP of client VPN 
+        # device_name = null
+        # bgp_asn = 65000
+        # bgp_asn_extended = null
+        # certificate_arn = null
+
+        # customer_gateway_id = null
+      }
+
+      # Tunnel resource settings; if omitted, null/default values apply
+      vpn_connection = {
+        remote_ipv4_network_cidr = "10.30.0.0/16" // CIDR block shared from our VPC
+
+        # If using static routing for specific machines, use
+        static_routes_only         = true
+        # static_routes_destinations = ["10.50.0.0/16", "10.51.0.0/16"]
+        # route_table_names          = ["dmc-lv1-private", "dmc-lv1-public"]
+
+        tunnel1_preshared_key                = "12345678" # local.secrets.vpn_preshared_key
+        tunnel1_ike_versions                 = ["ikev2"]
+        tunnel1_startup_action               = "start"
+        tunnel1_dpd_timeout_action           = "none"
+        tunnel1_phase1_dh_group_numbers      = ["14"]
+        tunnel1_phase1_encryption_algorithms = ["AES128"]
+        tunnel1_phase1_integrity_algorithms  = ["SHA2-256"]
+        tunnel1_phase2_dh_group_numbers      = ["14"]
+        tunnel1_phase2_encryption_algorithms = ["AES256"]
+        tunnel1_phase2_integrity_algorithms  = ["SHA2-256"]
+        tunnel1_cloudwatch_log_enabled       = true
+
+        tunnel2_preshared_key                = "12345678" # local.secrets.vpn_preshared_key
+        tunnel2_ike_versions                 = ["ikev2"]
+        tunnel2_startup_action               = "start"
+        tunnel2_dpd_timeout_action           = "none"
+        tunnel2_phase1_dh_group_numbers      = ["14"]
+        tunnel2_phase1_encryption_algorithms = ["AES128"]
+        tunnel2_phase1_integrity_algorithms  = ["SHA2-256"]
+        tunnel2_phase2_dh_group_numbers      = ["14"]
+        tunnel2_phase2_encryption_algorithms = ["AES256"]
+        tunnel2_phase2_integrity_algorithms  = ["SHA2-256"]
+        tunnel2_cloudwatch_log_enabled       = true
+      }
+
+      # # Route propagation and route configuration when not using static routing only
+      # vpc_routes = {
+      #   "private" = {
+      #     destination_cidr = ["172.0.10.1/24", "172.0.10.2/24"]
+      #   }
+      #   "public" = {
+      #     destination_cidr = ["172.0.10.1/24", "172.0.10.2/24"]
+      #   }
+      # }
+    }
+  }
 }
